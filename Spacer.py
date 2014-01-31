55ac2a08e481f6a42349b68a391a884704bb5b52
komal-rathore <komal.rathore40@gmail.com>
2014-01-31 22:03:36 +0530

    Adding comments to the file

--- Spacer.py
+++ Spacer.py
@@ -1,11 +1,16 @@
 #Program for replaceing multiple space with Single space
+#this file is source file
 f = open('filespace.txt', 'r')
+# Read Contents of target files
 st=f.read()
 print "Read String ",st
+
+#this file is target file
 ws=open("filewithoutspaceFor3.txt",'w')
 
 print "After Remove Space"
 
+#Write content of target file with replacing multiple single space into single one
 for x in range(0,len(st)):
     if st[x]==" " and x+1 < len(st):
         if st[x+1]==" ":
